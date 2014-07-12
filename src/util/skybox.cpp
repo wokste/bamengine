d232c0aa244c31856853b69f733d20248d7a57e1
Steven Wokke <wokste@users.noreply.github.com>
2014-07-12 17:15:27 +0200

    Fixed #18

--- src/util/skybox.cpp
+++ src/util/skybox.cpp
@@ -11,11 +11,11 @@ Skybox::Skybox(){
 	mPhaseColor[Day] = sf::Color(128,192,255);
 	mPhaseColor[Night] = sf::Color(0,32,64);
 
-	int ticksPerMinute = 25 * 60;
+	int ticksPerMinute = 25 * 30;
 	mPhaseTime[Day] = 5 * ticksPerMinute;
 	mPhaseTime[Night] = 6 * ticksPerMinute;
-	mTicks = 0;
-	mInterpolationTime = ticksPerMinute * 0.5;
+	mTicks = mPhaseTime[Day];
+	mInterpolationTime = ticksPerMinute * 1;
 }
 
 void Skybox::tick(){
@@ -33,13 +33,28 @@ Skybox::Phase Skybox::getPhase() const{
 }
 
 void Skybox::render(sf::RenderTarget& window) const{
+	renderSky(window);
+
+	if (getPhase() == Phase::Day){
+		sf::IntRect rect(0,0,128,128);
+		renderSun(window, rect, 0, mPhaseTime[Phase::Day] - mInterpolationTime);
+	} else {
+		sf::IntRect rect(128,0,128,128);
+		renderSun(window, rect, mPhaseTime[Phase::Day] - mInterpolationTime, mPhaseTime[Phase::Night]);
+	}
+
+	renderTerrain(window, 128, 384, 0.3);
+	renderTerrain(window, 128, 256, 0.6);
+}
+
+void Skybox::renderSky(sf::RenderTarget& window) const{
 	Phase phase = getPhase();
 
 	if (mTicks + mInterpolationTime >= mPhaseTime[phase]){
 		auto& oldColor = mPhaseColor[phase];
 		auto& newColor = mPhaseColor[(phase + 1) % Phase::Max];
 
-		double interpolation = 1 - (mPhaseTime[phase] - mTicks) / static_cast<double>(mInterpolationTime);
+		double interpolation = 1 - static_cast<double>(mPhaseTime[phase] - mTicks) / static_cast<double>(mInterpolationTime);
 
 		window.clear(sf::Color(
 			MathPlus::interpolate(oldColor.r, newColor.r, interpolation),
@@ -48,23 +63,54 @@ void Skybox::render(sf::RenderTarget& window) const{
 	} else {
 		window.clear(mPhaseColor[phase]);
 	}
+}
 
+void Skybox::renderTerrain(sf::RenderTarget& window, int imgTop, int imgHeight, double scrollSpeed) const{
 	// Draw the terain
 	sf::Sprite sprite;
 	sprite.setTexture(*mTexture);
 
 	sf::Vector2u windowSize = window.getSize();
 
-	int terrainHeight = std::min(384, static_cast<int>(windowSize.y / 2));
+	int terrainHeight = std::min(imgHeight, static_cast<int>(windowSize.y / 2));
 
 	sf::Vector2f worldPos = window.mapPixelToCoords(sf::Vector2i(0,windowSize.y - terrainHeight ));
 
 	sprite.setPosition(worldPos);
 	sprite.setScale(sf::Vector2f(0.5f, 0.5f));
 
-	int skyLeft = static_cast<int>(window.getView().getCenter().x * 0.5);
+	int skyLeft = static_cast<int>(window.getView().getCenter().x * scrollSpeed);
 
-	sprite.setTextureRect(sf::IntRect(skyLeft, 128, windowSize.x, 384));
+	sprite.setTextureRect(sf::IntRect(skyLeft, imgTop, windowSize.x, imgHeight));
 
 	window.draw(sprite);
 }
+
+void Skybox::renderSun(sf::RenderTarget& window, const sf::IntRect& rect, int tickStart, int tickEnd) const{
+	if (mTicks < tickStart || mTicks > tickEnd)
+		return;
+
+	double time = static_cast<double>(mTicks - tickStart) / static_cast<double>(tickEnd - tickStart);
+
+	sf::Sprite sprite;
+	sprite.setTexture(*mTexture);
+	sprite.setTextureRect(rect);
+	sprite.setOrigin(64,64);
+	float scale = 0.3f;
+	sprite.setScale(scale, scale);
+
+	constexpr double pi = 3.141592;
+
+	sf::Vector2u windowSize = window.getSize();
+
+	sf::Vector2i center(windowSize.x / 2, windowSize.y - 200);
+	sf::Vector2f radius(windowSize.x * 0.4, windowSize.x * 0.2);
+	double angle = pi * (time) + pi;
+	sf::Vector2i translate(std::cos(angle) * radius.x, std::sin(angle) * radius.y);
+	sf::Vector2f worldPos = window.mapPixelToCoords(center + translate);
+
+	sprite.setPosition(worldPos);
+
+	window.draw(sprite);
+}
+
--- src/util/skybox.h
+++ src/util/skybox.h
@@ -23,4 +23,8 @@ public:
 	Phase getPhase() const;
 
 	void tick();
+private:
+	void renderSky(sf::RenderTarget& window) const;
+	void renderTerrain(sf::RenderTarget& window, int top, int height, double speed) const;
+	void renderSun(sf::RenderTarget& window, const sf::IntRect& rect, int tickStart, int tickEnd) const;
 };
