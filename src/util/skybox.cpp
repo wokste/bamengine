fbe6f23b42d34d23a7ef02fabbf5c36c751e7722
Steven Wokke <wokste@users.noreply.github.com>
2014-07-12 13:14:08 +0200

    Added terrain in the Skybox

--- src/util/skybox.cpp
+++ src/util/skybox.cpp
@@ -1,14 +1,16 @@
 #include "skybox.h"
 #include <SFML/Graphics/RenderTarget.hpp>
+#include <SFML/Graphics/Sprite.hpp>
 
 Skybox::Skybox(){
 	std::string filename = "data/skybox_plains.png";
 	mTexture = std::make_shared<sf::Texture>();
 	mTexture->loadFromFile(filename);
+	mTexture->setRepeated(true);
 	mPhaseColor[Day] = sf::Color(128,192,255);
 	mPhaseColor[Night] = sf::Color(0,32,64);
 
-	int ticksPerMinute = 25;
+	int ticksPerMinute = 25 * 6;
 	mPhaseTime[Day] = 4 * ticksPerMinute;
 	mPhaseTime[Night] = 5 * ticksPerMinute;
 }
@@ -27,8 +29,27 @@ Skybox::Phase Skybox::getPhase() const{
 	return Night;
 }
 
-void Skybox::render(sf::RenderTarget& target) const{
+void Skybox::render(sf::RenderTarget& window) const{
 	Phase phase = getPhase();
 
-	target.clear(mPhaseColor[phase]);
+	window.clear(mPhaseColor[phase]);
+
+	// Draw the terain
+	sf::Sprite sprite;
+	sprite.setTexture(*mTexture);
+
+	sf::Vector2u windowSize = window.getSize();
+
+	int terrainHeight = std::min(384, static_cast<int>(windowSize.y / 2));
+
+	sf::Vector2f worldPos = window.mapPixelToCoords(sf::Vector2i(0,windowSize.y - terrainHeight ));
+
+	sprite.setPosition(worldPos);
+	sprite.setScale(sf::Vector2f(0.5f, 0.5f));
+
+	int skyLeft = static_cast<int>(window.getView().getCenter().x * 0.5);
+
+	sprite.setTextureRect(sf::IntRect(skyLeft, 128, windowSize.x, 384));
+
+	window.draw(sprite);
 }
